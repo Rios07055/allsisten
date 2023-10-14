32050d2ed3687b97f82704743b52968caffc78a4
VictorVidal12 <110042484+VictorVidal12@users.noreply.github.com>
2023-10-13 22:25:44 -0500

    Solucion #2 del taller.py

--- Taller de algoritmos voraces/Solucion #2 del taller.py
+++ Taller de algoritmos voraces/Solucion #2 del taller.py
@@ -5,16 +5,24 @@ n = 7
 def operaciones(N, valor=0, resultado=0):
   if N == valor:
     return resultado
-  else:
-    if valor > N:
-      resultado -= 1
-      valor /= 2
+  elif N%2 == 0:
+    if valor == 0:
       return operaciones(N, valor+1, resultado + 1)
-    elif valor != 0:
+    elif N == valor*2:
       return operaciones(N, valor*2, resultado + 1)
-    elif valor == 0:
+    elif N - valor > valor*2:
+      return operaciones(N, valor*2, resultado + 1)
+    else:
       return operaciones(N, valor+1, resultado + 1)
-
+  else:
+    if valor == 0:
+      return operaciones(N, valor+1, resultado + 1)
+    elif N-1 == valor*2:
+      return operaciones(N, valor*2, resultado + 1)
+    elif (N-1) - valor > valor*2:
+      return operaciones(N, valor*2, resultado + 1)
+    else:
+      return operaciones(N, valor+1, resultado + 1)
 
 
 print(operaciones(N))
